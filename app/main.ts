81f605e78e860f5dcd1d2484d61bd0ee9aab1d0b
rubencustodio <rcustodio@spokeo.com>
2017-03-26 09:18:59 -0700

    removing prodmore for now

--- app/main.ts
+++ app/main.ts
@@ -1,11 +1,7 @@
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
-import {enableProdMode} from '@angular/core';
 
 import { AppModule } from './components/app.module';
 
 import './styles/main.scss';
 
-if (window.location.href = 'http://rubencustodio.com/') {
-    enableProdMode();
-}
 platformBrowserDynamic().bootstrapModule(AppModule);
